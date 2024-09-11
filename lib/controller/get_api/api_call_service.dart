99e480fc45b9f3af2b8989205b8d9c238d5734dc
S <shakibmohammad18@gmail.com>
2024-09-11 15:22:55 +0600

    project upgrade

--- lib/controller/get_api/api_call_service.dart
+++ lib/controller/get_api/api_call_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:get_api_project/model/get_model/commad_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,7 +7,6 @@ class GetApiService {
 
   static Future<List<CommentModel>?> getComments() async {
     final response = await http
-
         .get(Uri.parse("https://jsonplaceholder.typicode.com/comments"));
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
